81498ecfd2df654eb127c8cea494e6b7508189d6
seongho <92960654+kimseongho920102@users.noreply.github.com>
2024-11-25 13:14:14 +0000

    test

--- remix/remix.config.js
+++ remix/remix.config.js
@@ -1,7 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverBuildTarget: "browser",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/template/build/", // 저장소명 반영
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build", // 빌드 결과물 디렉토리
+  serverBuildPath: "build/index.js",
+  publicPath: "/build/",
+  future: {
+    v2_meta: true,
+  },
 };
